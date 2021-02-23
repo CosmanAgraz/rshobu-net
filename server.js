b39a98c4aa9944365466fc5c483c68c8669c6fbb
CosmanAgraz <im.serge@hotmail.com>
2021-02-23 00:50:22 -0500

    F*ck separation of concerns (backend/frontend use the same package.json)

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = new express();
+
+var port = process.env.PORT || 3000;
+
+app.set("PORT", port);
+
+app.use(express.static("./build"));
+
+const entryPoint = path.resolve(__dirname, "build", "index.html");
+
+app.get("*", (req, res) => 
+{
+    res.sendFile(path.resolve( entryPoint ));
+ });
+
+app.listen(port, () => console.log("listen on this port: " + port));
