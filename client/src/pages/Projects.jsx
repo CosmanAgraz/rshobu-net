085630e7b37c80d1ed2d32b410c0dc830e7fb4d4
CosmanAgraz <im.serge@hotmail.com>
2021-06-16 04:12:51 -0400

    Added Projects page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -16,13 +16,17 @@ const Projects = () =>
             <Text classname="p">
                 Looking for a fun way to play Megaman Battle Network 3?  Try this randomizer!! Contributed to the user interface of this project!
             </Text>
+
             <Text classname="p">
                 Credits: XKirby, Mountebank/Samfin, Dabomstew, Prof.9, Rockman.EXE and TeamBN
             </Text>
+
             <Text type="h4">
                 <Link to="https://discord.com/api/oauth2/authorize?client_id=381961028486823939&permissions=67584&scope=bot">Shyvana-bot</Link>
             </Text>
+
             <Image source="https://sharedassets39.blob.core.windows.net/resources/sample.PNG">Friendship preservation is not guaranteed</Image>
+            
             <Text classname="p">
                 A <Link to="https://discord.com/">Discord</Link> bot I made for generating brave (random) <Link to="https://leagueoflegends.com/">League of Legends</Link> builds directly to your Discord server.
             </Text>
